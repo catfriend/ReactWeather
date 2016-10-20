9f3be6ddcf30b19219b839181af3620fa3fed82f
catfriend <eakari@live.com>
2016-10-20 11:34:58 -0700

    added grid system

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,22 @@
 var React = require('react');
+var {Link} =require('react-router');
 
 // refactor to arrow with everything from the render function
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examle locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Seattle'>Seattle, WA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Nairobi'>Nairobi, Kenya</Link>
+
+        </li>
+      </ol>
     </div>
   )
 }
