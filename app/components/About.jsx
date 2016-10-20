065d5cb697b3552d0040629dc47f346e204c3883
catfriend <eakari@live.com>
2016-10-20 11:47:44 -0700

    added content to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,8 +13,23 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About Page!</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application built with the React.js framework.</p>
+      <p>Tools used for this project include:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This is the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I linked to Open Weather Map's API to search for weather data by city name.
+        </li>
+        <li>
+          <a href="https://github.com/webpack/webpack">Webpack</a> - I used webpack for my bundler.
+        </li>
+        <li>
+          <a href="http://foundation.zurb.com/">Foundation</a> - I used Foundation for styling.
+        </li>
+      </ul>
     </div>
   )
 };
