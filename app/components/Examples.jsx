8541572cbd8438ea4f0e9d57b72422267d0dcc48
catfriend <eakari@live.com>
2016-10-19 17:29:50 -0700

    text changes

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,7 +4,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h3>Examples</h3>
+    <div>
+      <h3>Examples</h3>
+      <p>Welcome to examples page!</p>
+    </div>
   )
 }
 
