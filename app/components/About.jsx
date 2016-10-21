32247c428a92cb77cb937ce98eaeba3d215e1f8b
catfriend <eakari@live.com>
2016-10-21 11:11:10 -0700

    added styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,7 +13,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>This is a weather application built with the React.js framework.</p>
       <p>Tools used for this project include:</p>
       <ul>
